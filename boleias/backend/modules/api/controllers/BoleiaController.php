52b1f0145aa6d5a4588879752b1e5e83fc9d8b88
André Inácio <146387235+2231445@users.noreply.github.com>
2025-10-22 00:00:29 +0100

    CCS 18 - Configuração Iniciais
    - Configuração de "Pretty URLs";
    - Configuração dos módulos de API

---
+++ boleias/backend/modules/api/controllers/BoleiaController.php
@@ -0,0 +1,8 @@
+<?php
+namespace backend\modules\api\controllers;
+
+use yii\rest\ActiveController;
+class BoleiaController extends ActiveController
+{
+    public $modelClass = 'common\models\Boleia';
+}
