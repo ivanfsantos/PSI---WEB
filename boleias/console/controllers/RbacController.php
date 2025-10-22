1fb976e328df57567c8a14c1d46ab232ac469c4d
André Inácio <146387235+2231445@users.noreply.github.com>
2025-10-22 23:34:06 +0100

    CCS 18 - Permissões de modelos
    - Permissões de CRUD dos modelos da base de dados

--- boleias/console/controllers/RbacController.php
+++ boleias/console/controllers/RbacController.php
@@ -11,6 +11,109 @@ class RbacController extends Controller
     {
         $auth = Yii::$app->authManager;
 
+        // Limpar todos os papéis e permissões para resetar o RBAC
+        $auth->removeAll();
+
+        // ==== Definir Permissões ====
+
+        //Passageiro
+        $criarPassageiro = $auth->createPermission('criarPassageiro');
+        $criarPassageiro->description = 'Criar um  perfil de Passageiro';
+        $auth->add($criarPassageiro);
+
+        $editarPassageiro = $auth->createPermission('editarPassageiro');
+        $editarPassageiro->description = 'Editar um  perfil de Passageiro';
+        $auth->add($editarPassageiro);
+
+        $eliminarPassageiro = $auth->createPermission('eliminarPassageiro');
+        $eliminarPassageiro->description = 'Eliminar um  perfil de Passageiro';
+        $auth->add($eliminarPassageiro);
+
+        //Condutor
+        $criarCondutor = $auth->createPermission('criarCondutor');
+        $criarCondutor->description = 'Criar um  perfil de Condutor';
+        $auth->add($criarCondutor);
+
+        $editarCondutor = $auth->createPermission('editarCondutor');
+        $editarCondutor->description = 'Editar um perfil de Condutor';
+        $auth->add($editarCondutor);
+
+        $eliminarCondutor = $auth->createPermission('eliminarCondutor');
+        $eliminarCondutor->description = 'Eliminar um perfil de Condutor';
+        $auth->add($eliminarCondutor);
+
+        //Admin
+        $criarAdmin = $auth->createPermission('criarAdmin');
+        $criarAdmin->description = 'Criar um  perfil de Administrador';
+        $auth->add($criarAdmin);
+
+        $editarAdmin = $auth->createPermission('editarAdmin');
+        $editarAdmin->description = 'Editar um perfil de Administrador';
+        $auth->add($editarAdmin);
+
+        $eliminarAdmin = $auth->createPermission('eliminarAdmin');
+        $eliminarAdmin->description = 'Eliminar um perfil de Administrador';
+        $auth->add($eliminarAdmin);
+
+        //Documentos
+        $criarDocumento = $auth->createPermission('criarDocumento');
+        $criarDocumento->description = 'Criar um Documento';
+        $auth->add($criarDocumento);
+
+        $editarDocumento = $auth->createPermission('editarDocumento');
+        $editarDocumento->description = 'Editar um Documento';
+        $auth->add($editarDocumento);
+
+        $eliminarDocumento = $auth->createPermission('eliminarDocumento');
+        $eliminarDocumento->description = 'Eliminar um Documento';
+        $auth->add($eliminarDocumento);
+
+        //Avaliações
+        $criarAvaliacao = $auth->createPermission('criarAvaliacao');
+        $criarAvaliacao->description = 'Criar uma Avaliacao';
+        $auth->add($criarAvaliacao);
+
+        $editarAvaliacao = $auth->createPermission('editarAvaliacao');
+        $editarAvaliacao->description = 'Editar uma Avaliacao';
+        $auth->add($editarAvaliacao);
+
+        $eliminarAvaliacao = $auth->createPermission('eliminarAvaliacao');
+        $eliminarAvaliacao->description = 'Eliminar uma Avaliacao';
+        $auth->add($eliminarAvaliacao);
+
+        //Reservas
+        $criarReserva = $auth->createPermission('criarReserva');
+        $criarReserva->description = 'Criar uma Reserva';
+        $auth->add($criarReserva);
+
+        $cancelarReserva = $auth->createPermission('cancelarReserva');
+        $cancelarReserva->description = 'Cancelar uma Reserva';
+        $auth->add($cancelarReserva);
+
+        //Viaturas
+        $criarViatura = $auth->createPermission('criarViatura');
+        $criarViatura->description = 'Criar uma Viatura';
+        $auth->add($criarViatura);
+
+        $editarViatura = $auth->createPermission('editarViatura');
+        $editarViatura->description = 'Editar uma Viatura';
+        $auth->add($editarViatura);
+
+        $eliminarViatura = $auth->createPermission('eliminarViatura');
+        $eliminarViatura->description = 'Eliminar uma Viatura';
+        $auth->add($eliminarViatura);
+
+        //Destinos Favoritos
+        $adicionarFavorito = $auth->createPermission('adicionarFavorito');
+        $adicionarFavorito->description = 'Adicionar um destino favorito';
+        $auth->add($adicionarFavorito);
+
+        $removerFavorito = $auth->createPermission('removerFavorito');
+        $removerFavorito->description = 'Remover um destino favorito';
+        $auth->add($removerFavorito);
+
+
+
 
         // adciona a role "passageiro"
         $passageiro = $auth->createRole('passageiro');
