59507dbf828532cc33f5f85ad512ba43aa8f5ba3
tiagolhbernardino <2231448@my.ipleiria.pt>
2025-11-05 13:23:54 +0000

    Crud documentos + upload

--- boleias/frontend/views/documento/index.php
+++ boleias/frontend/views/documento/index.php
@@ -7,7 +7,7 @@ use yii\grid\ActionColumn;
 use yii\grid\GridView;
 
 /** @var yii\web\View $this */
-/** @var common\models\DocumentoSearch $searchModel */
+/** @var frontend\models\DocumentoSearch $searchModel */
 /** @var yii\data\ActiveDataProvider $dataProvider */
 
 $this->title = 'Documentos';
