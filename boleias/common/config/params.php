2a8acb7a6a5608371c037d2c792e6842a7b7821c
André Inácio <146387235+AndreFInacio@users.noreply.github.com>
2026-01-08 12:23:09 +0000

    alterações de backend

--- boleias/common/config/params.php
+++ boleias/common/config/params.php
@@ -7,7 +7,7 @@ return [
     'user.passwordResetTokenExpire' => 3600,
     'user.passwordMinLength' => 8,
     // Absolute URL to frontend web root (adjust if you serve under a different host/path)
-    'frontendBaseUrl' => 'http://localhost/PROJETOS/boleias/web/PSI-WEB/boleias/frontend/web',
+    'frontendBaseUrl' => 'http://localhost/Projeto/PSI---WEB/boleias/frontend/web',
     // Absolute URL to backend web root (adjust for production)
-    'backendBaseUrl' => 'http://localhost/PROJETOS/boleias/web/PSI-WEB/boleias/backend/web',
+    'backendBaseUrl' => 'http://localhost/Projeto/PSI---WEB/boleias/backend/web',
 ];
